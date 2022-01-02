5d8c8d1082079bce27f79f83fcde1c2584dff1d7
Rainbow <2180502841@qq.com>
2022-01-02 14:33:10 +0800

    update

--- packages/browse.py
+++ packages/browse.py
@@ -84,57 +84,56 @@ class browse_window(tk.Tk):
         except:
             pass
         try:
-            if self.parent.track_ind_get is not None:
-                read_mode = ''
-            else:
-                read_mode = 'find'
+            try:
+                all_tracks = mp.read(self.parent.file_path,
+                                     get_off_drums=piano_config.get_off_drums)
+            except:
+                all_tracks = mp.read(self.parent.file_path,
+                                     get_off_drums=piano_config.get_off_drums,
+                                     split_channels=True)
+            all_tracks.normalize_tempo()
+            current_bpm = all_tracks.bpm
             if not self.parent.if_merge:
-                self.parent.read_result = mp.read(self.parent.file_path,
-                                                  self.parent.track_ind_get,
-                                                  read_mode)
-                whole_notes = self.parent.read_result[1]
-                for each in whole_notes:
-                    each.own_color = piano_config.bar_color
-                normalize_result = self.parent.read_result[1].normalize_tempo(
-                    self.parent.read_result[0],
-                    start_time=self.parent.read_result[2])
-                if normalize_result:
-                    self.parent.read_result[2] = normalize_result[1]
+                if self.parent.track_ind_get is not None:
+                    all_tracks = [
+                        (current_bpm,
+                         all_tracks.tracks[self.parent.track_ind_get],
+                         all_tracks.start_times[self.parent.track_ind_get])
+                    ]
+                else:
+                    all_tracks = [(current_bpm, all_tracks.tracks[0],
+                                   all_tracks.start_times[0])]
+                all_tracks[0][1].reset_track(0)
             else:
-                try:
-                    all_tracks = mp.read(
-                        self.parent.file_path,
-                        self.parent.track_ind_get,
-                        mode='all',
-                        get_off_drums=piano_config.get_off_drums,
-                        to_piece=True)
-                except:
-                    all_tracks = mp.read(
-                        self.parent.file_path,
-                        self.parent.track_ind_get,
-                        mode='all',
-                        get_off_drums=piano_config.get_off_drums,
-                        to_piece=True,
-                        split_channels=True)
-                all_tracks.normalize_tempo()
-                all_tracks = [(all_tracks.bpm, all_tracks.tracks[i],
+                all_tracks = [(current_bpm, all_tracks.tracks[i],
                                all_tracks.start_times[i])
                               for i in range(len(all_tracks.tracks))]
-                pitch_bends = mp.concat([
-                    i[1].split(mp.pitch_bend, get_time=True)
-                    for i in all_tracks
-                ])
-                for each in all_tracks:
-                    each[1].clear_pitch_bend('all')
-                start_time_ls = [j[2] for j in all_tracks]
-                first_track_ind = start_time_ls.index(min(start_time_ls))
-                all_tracks.insert(0, all_tracks.pop(first_track_ind))
-                if piano_config.use_track_colors:
-                    color_num = len(all_tracks)
-                    import random
-                    if not piano_config.use_default_tracks_colors:
-                        colors = []
-                        for i in range(color_num):
+
+            pitch_bends = mp.concat(
+                [i[1].split(mp.pitch_bend, get_time=True) for i in all_tracks])
+            for each in all_tracks:
+                each[1].clear_pitch_bend('all')
+            start_time_ls = [j[2] for j in all_tracks]
+            first_track_ind = start_time_ls.index(min(start_time_ls))
+            all_tracks.insert(0, all_tracks.pop(first_track_ind))
+            if piano_config.use_track_colors:
+                color_num = len(all_tracks)
+                import random
+                if not piano_config.use_default_tracks_colors:
+                    colors = []
+                    for i in range(color_num):
+                        current_color = tuple(
+                            [random.randint(0, 255) for j in range(3)])
+                        while (colors == (255, 255, 255)) or (current_color
+                                                              in colors):
+                            current_color = tuple(
+                                [random.randint(0, 255) for j in range(3)])
+                        colors.append(current_color)
+                else:
+                    colors = piano_config.tracks_colors
+                    colors_len = len(colors)
+                    if colors_len < color_num:
+                        for k in range(color_num - colors_len):
                             current_color = tuple(
                                 [random.randint(0, 255) for j in range(3)])
                             while (colors == (255, 255, 255)) or (current_color
@@ -142,37 +141,22 @@ class browse_window(tk.Tk):
                                 current_color = tuple(
                                     [random.randint(0, 255) for j in range(3)])
                             colors.append(current_color)
-                    else:
-                        colors = piano_config.tracks_colors
-                        colors_len = len(colors)
-                        if colors_len < color_num:
-                            for k in range(color_num - colors_len):
-                                current_color = tuple(
-                                    [random.randint(0, 255) for j in range(3)])
-                                while (colors
-                                       == (255, 255, 255)) or (current_color
-                                                               in colors):
-                                    current_color = tuple([
-                                        random.randint(0, 255)
-                                        for j in range(3)
-                                    ])
-                                colors.append(current_color)
-                first_track = all_tracks[0]
-                tempo, all_track_notes, first_track_start_time = first_track
-                for i in range(len(all_tracks)):
-                    current = all_tracks[i]
-                    current_track = current[1]
-                    if piano_config.use_track_colors:
-                        current_color = colors[i]
-                        for each in current_track:
-                            each.own_color = current_color
-                    if i > 0:
-                        all_track_notes &= (current_track, current[2] -
-                                            first_track_start_time)
-                all_track_notes += pitch_bends
-                if self.parent.set_bpm != '':
-                    tempo = float(self.parent.set_bpm)
-                self.parent.read_result = tempo, all_track_notes, first_track_start_time
+            first_track = all_tracks[0]
+            tempo, all_track_notes, first_track_start_time = first_track
+            for i in range(len(all_tracks)):
+                current = all_tracks[i]
+                current_track = current[1]
+                if piano_config.use_track_colors:
+                    current_color = colors[i]
+                    for each in current_track:
+                        each.own_color = current_color
+                if i > 0:
+                    all_track_notes &= (current_track,
+                                        current[2] - first_track_start_time)
+            all_track_notes += pitch_bends
+            if self.parent.set_bpm != '':
+                tempo = float(self.parent.set_bpm)
+            self.parent.read_result = tempo, all_track_notes, first_track_start_time
 
         except Exception as e:
             print(str(e))
@@ -239,6 +223,7 @@ class browse_window(tk.Tk):
                                                variable=self.if_melody)
             self.main_melody.place(x=-60, y=180, width=300, height=30)
             self.if_merge_all_tracks = tk.IntVar()
+            self.if_merge_all_tracks.set(1)
             self.merge_all_tracks = ttk.Checkbutton(
                 self.labelFrame,
                 text=self.browse_dict['merge'],
@@ -263,6 +248,6 @@ class setup:
         self.sheetlen = None
         self.set_bpm = None
         self.off_melody = 0
-        self.if_merge = False
+        self.if_merge = True
         self.current_browse_window = browse_window(self, browse_dict)
         self.current_browse_window.mainloop()
